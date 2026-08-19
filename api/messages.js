46bbb6528d09538c650ef84685815f344bf9fe2d
Sulemansaqib243342 <sulemansaqib34917@gmail.com>
2026-08-19 15:41:34 +0500

    fix: revert Prisma to v5 syntax

--- api/messages.js
+++ api/messages.js
@@ -5,7 +5,7 @@
 
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({ adapter: process.env.POSTGRES_URL });
+const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
